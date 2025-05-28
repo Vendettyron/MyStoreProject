007cad1415e63b5e757ca21dacebafe5332e369d
Byron Coc <cocpalomobyronalejandro@gmail.com>
2025-05-27 21:40:35 -0600

    .
    .

--- backend/src/apps/almacen/interfaces/http/routes/materiaPrima.routes.ts
+++ backend/src/apps/almacen/interfaces/http/routes/materiaPrima.routes.ts
@@ -1,14 +1,12 @@
 import { permisoMiddleware } from "src/apps/almacen/infrastructure/middlewares/permissionMiddlware";
 import { permiso } from "src/apps/almacen/shared/diccionario/permisosDiccionario";
 import { rol } from "src/apps/almacen/shared/diccionario/rolDiccionario";
-import { productoSchema } from "src/apps/almacen/shared/schemas/producto.schema";
+import { productoSchema } from "src/apps/almacen/shared/schemas/productos/producto.schema";
 import { authMiddleware } from "src/shared/middlewares/authMiddleware";
 import { roleMiddleware } from "src/shared/middlewares/roleMiddleware";
 import { basicResponseSchema } from "src/shared/schemas/messageResponseSchema";
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { createProductoController } from "../controllers/proucto.controller";
-
-import { crearMateriaPrima } from "../controllers/materiaPrima.controller";
 
 export default async function materiaPrimaRoutes(app: FastifyInstance) {
 	app.post("/crear-materia-prima", {
