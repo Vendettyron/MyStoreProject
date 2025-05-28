f65cf01a1e6716495ea7972bca47cb009c68b9ee
Byron Coc <cocpalomobyronalejandro@gmail.com>
2025-05-27 21:54:40 -0600

    .
    .

--- backend/src/apps/almacen/domain/repositories/materia_prima/materiaPrimaRepository.ts
+++ backend/src/apps/almacen/domain/repositories/materia_prima/materiaPrimaRepository.ts
@@ -1,11 +1,12 @@
 import type { MateriaPrima } from "./materiaPrimaEntity";
 
 export interface IMateriaPrimaRepository {
-    crearMateriaPrima(data: MateriaPrima): Promise<{ success: boolean; message: string}>;
-    obtenerMateriasPrimas(): Promise<{
-        success: boolean;
-        message: string;
-        data?: object;
-    }>;
+	crearMateriaPrima(
+		data: MateriaPrima,
+	): Promise<{ success: boolean; message: string }>;
+	// obtenerMateriasPrimas(): Promise<{
+	// 	success: boolean;
+	// 	message: string;
+	// 	data?: object;
+	// }>;
 }
-
